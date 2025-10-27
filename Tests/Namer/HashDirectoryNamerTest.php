f1f05ca365512d10a3a1c8f2bba386589e99b450
Nathanael d. Noblet <nathanael@noblet.ca>
2025-10-27 09:42:03 -0600

    Bump phpunit version

--- Tests/Namer/HashDirectoryNamerTest.php
+++ Tests/Namer/HashDirectoryNamerTest.php
@@ -3,22 +3,19 @@
 namespace NS\FileUploadBundle\Tests\Namer;
 
 use NS\FileUploadBundle\Namer\HashDirectoryNamer;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class HashDirectoryNamerTest extends TestCase
 {
-    /**
-     * @param $data
-     *
-     * @dataProvider getDirectoryNames
-     */
-    public function testGenerate($data): void
+    #[DataProvider('getDirectoryNames')]
+    public function testGenerate(string $data): void
     {
         $namer = new HashDirectoryNamer();
         self::assertEquals(sha1($data),$namer->getDirectory($data));
     }
 
-    public function getDirectoryNames(): array
+    public static function getDirectoryNames(): array
     {
         return [
             ['something'],
